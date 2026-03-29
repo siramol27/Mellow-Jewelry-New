c689598a8a06e1133cb7d8000e883536b42339d3
siramol27 <siramonkimmilk@gmail.com>
2026-03-29 15:47:05 +0700

    fix NODE_ENV

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -30,8 +30,8 @@ export const register = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.node_env === 'production',
-            sameSite: process.env.node_env === 'production' ? 'none' : 'lax',
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
@@ -75,8 +75,8 @@ export const login = async (req , res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.node_env === 'production',
-            sameSite: process.env.node_env === 'production' ? 'none' : 'lax',
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
@@ -102,8 +102,8 @@ export const logout = async (req,res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.node_env === 'production',
-            sameSite: process.env.node_env === 'production' ? 'none' : 'lax',
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         })
 
         return res.json({success: true, message: "ออกจากระบบสำเร็จ"})
